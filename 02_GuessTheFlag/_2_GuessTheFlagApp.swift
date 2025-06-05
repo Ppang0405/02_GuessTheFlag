bfe545f62c1391cdf905a679ab1edba90a22e6c8
truongnguyen3 <truong.nguyen3@niteco.se>
2025-06-05 14:49:50 +0700

    Initial Commit

---
+++ 02_GuessTheFlag/_2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_GuessTheFlagApp.swift
+//  02_GuessTheFlag
+//
+//  Created by Truong Huu Nguyen on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct _2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
